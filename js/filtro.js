a99370352c90734e46cdfde7226285eb9cb9e256
Emmanuel Luna <emmanueleluna@gmail.com>
2025-01-24 14:56:29 -0600

    corrección y revisión de  codigo

--- js/filtro.js
+++ js/filtro.js
@@ -1,15 +1,17 @@
 // Tenemos un li de productos
-
 const productos = [
-  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
-]
+  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "../img/zapato-negro.jpg"},
+  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "../img/taco-azul.jpg"},
+  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "../img/bota-negra.jpg"},
+  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "../img/bota-azul.jpg"},
+  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "../img/zapato-rojo.jpg"},
+];
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+// pone dos veces lista de productos, dejo la del id porque su atributo no es "name"
+const li = document.getElementById("lista-de-productos");
+//quito esta linea porque no hay ninguna clase "input"
+//Agrego correctamente el id del input
+const $i = document.getElementById("filtro-input");
 
 for (let i = 0; i < productos.length; i++) {
   var d = document.createElement("div")
@@ -28,7 +30,6 @@ for (let i = 0; i < productos.length; i++) {
   li.appendChild(d)
 }
 
-displayProductos(productos)
 const botonDeFiltro = document.querySelector("button");
 
 botonDeFiltro.onclick = function() {
@@ -38,7 +39,8 @@ botonDeFiltro.onclick = function() {
 
   const texto = $i.value;
   console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
+  //DEFINO que productos es un array
+  const productosFiltrados = Array.isArray(productos) ? filtrado(productos, texto) : [];
 
   for (let i = 0; i < productosFiltrados.length; i++) {
     var d = document.createElement("div")
